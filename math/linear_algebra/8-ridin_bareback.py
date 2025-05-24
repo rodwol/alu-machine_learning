b20a0913b6f3f288638007c822c231cbba2f1669
rodwol <r.goniche@alustudent.com>
2025-05-24 05:19:20 +0000

    fixed errors

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -8,7 +8,7 @@ def mat_mul(mat1, mat2):
         return None
 
     result = [[0 for j in range(len(mat2[0]))] for i
-             in range(len(mat1))]
+              in range(len(mat1))]
 
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
