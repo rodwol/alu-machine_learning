cdb19e657604d9ed47d415c7e3c88178a65db8e3
rodwol <r.goniche@alustudent.com>
2025-06-13 23:08:29 -0700

    fixed error

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,14 +4,15 @@
 
 def poly_derivative(poly):
     """ function that calculates the derivative of a polynomial """
-    if not isinstance(poly, list) or not all(isinstance(c, int, float)) for c in poly):
+    if not isinstance(poly, list):
+        return None
+    if not poly:
         return None
 
-    if len(poly) <= 1:
-        return [0]
-
-    derivative = [poly[i]* i for i in range(1, len(poly))]
-    if all(c == 0 for c in derivative):
+    derivative = []
+    for power in range(1, len(poly)):
+        derivative.append(power * poly[power])
+    if not derivative:
         return [0]
 
     return derivative
