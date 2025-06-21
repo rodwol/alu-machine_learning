f364bcc1b6e04af45d1bce056308da563ac0d005
rodwol <r.goniche@alustudent.com>
2025-06-20 22:40:14 -0700

    update

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -9,7 +9,7 @@ class Poisson:
         """ initializes the poisson distribution """
         if data is None:
             if lambtha <= 0:
-                raise ValueError("Lambda must be a positive value")
+                raise ValueError("lambda must be a positive value")
             self.lambtha = float(lambtha)
         else:
             if not isinstance(data, list):
@@ -17,3 +17,31 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculates the PMF for a given number of 'successes' k."""
+        k = int(k)
+        if k < 0:
+            return 0
+
+        e_term = math.exp(-self.lambtha)
+        lambtha_power_k = self.lambtha ** k
+        factorial_k = math.factorial(k)
+
+        pmf_value = (e_term * lambtha_power_k) / factorial_k
+        return pmf_value
+
+    def cdf(self, k):
+        """Calculates the CDF for a given number of 'successes' k."""
+        k = int(k)
+        if k < 0:
+            return 0
+
+        cdf_value = 0
+        for i in range(0, k + 1):
+            e_term = math.exp(-self.lambtha)
+            lambtha_power_i = self.lambtha ** i
+            factorial_i = math.factorial(i)
+            cdf_value += (e_term * lambtha_power_i) / factorial_i
+
+        return cdf_value
