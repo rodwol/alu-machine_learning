57e0904aaeaed48ac8cfb690ccffc07947a7e920
rodwol <r.goniche@alustudent.com>
2025-06-20 23:42:15 -0700

    fixed errors

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -24,11 +24,11 @@ class Exponential:
         if x < 0:
             return 0
         e = 2.7182818285
-        return self.lambtha * (e **( -self.lambtha * x))
+        return self.lambtha * (e ** (-self.lambtha * x))
 
     def cdf(self, x):
         """ calculates the value of cdf """
         if x < 0:
             return 0
         e = 2.7182818285
-        return 1 - (e ** ( -self.lambtha * x))
+        return 1 - (e ** (-self.lambtha * x))
