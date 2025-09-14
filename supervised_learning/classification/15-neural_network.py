04a24aa40e3afa619ecbf2410832f6134cd8075e
rodwol <r.goniche@alustudent.com>
2025-09-14 18:22:04 +0000

    fixed error

--- supervised_learning/classification/15-neural_network.py
+++ supervised_learning/classification/15-neural_network.py
@@ -81,7 +81,7 @@ class NeuralNetwork:
         """
         m = Y.shape[1]
         cost = -(1/m) * np.sum(
-            Y * np.log(A) + (1- Y) * np.log(1.0000001 - A)
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
         )
         return cost
 
