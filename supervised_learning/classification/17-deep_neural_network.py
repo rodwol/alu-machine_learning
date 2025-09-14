83ae0b5b7a0546e3dd4e5a485147a892ebee2b1f
rodwol <r.goniche@alustudent.com>
2025-09-14 21:34:04 +0000

    fixed error

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
+""" Deep Neural Network
 """
-Deep Neural Network class for binary classification
-"""
+
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network performing binary classification"""
+    """ Class that defines a deep neural network performing binary
+        classification.
+    """
 
     def __init__(self, nx, layers):
-        """
-        Constructor for DeepNeuralNetwork
-        """
+        """ Instantiation function
 
+        Args:
+            nx (int): number of input features
+            layers (list): representing the number of nodes in each layer of
+                           the network
+        """
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError('nx must be a positive integer')
 
-        if not isinstance(layers, list) or len(layers) == 0:
-            raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(x, int) and x > 0 for x in layers):
-            raise TypeError("layers must be a list of positive integers")
+        if not isinstance(layers, list):
+            raise TypeError('layers must be a list of positive integers')
+        if len(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
 
         self.__L = len(layers)
         self.__cache = {}
@@ -43,14 +48,18 @@ class DeepNeuralNetwork:
             # Zero initialization
             self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
 
+    # add getter method
     @property
     def L(self):
+        """ Return layers in the neural network"""
         return self.__L
 
     @property
     def cache(self):
+        """ Return dictionary with intermediate values of the network"""
         return self.__cache
 
     @property
     def weights(self):
+        """Return weights and bias dictionary"""
         return self.__weights
