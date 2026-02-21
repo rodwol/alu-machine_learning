1141f6b80142db057b986beb74a77f6ca4cee592
rodwol <r.goniche@alustudent.com>
2026-02-21 16:47:49 -0500

    Dimensionality Reduction

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+performs PCA on a dataset
+'''
+import numpy as np
+
+
+def pca(X, ndim):
+    '''
+    performs PCA on a dataset
+    '''
+    mean = np.mean(X, axis=0, keepdims=True)
+    A = X - mean
+    u, s, v = np.linalg.svd(A)
+    W = v.T[:, :ndim]
+    T = np.matmul(A, W)
+    return (T)
